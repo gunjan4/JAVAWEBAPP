ebca5dcea03a687033ba8011f012528970ef7083
ralph.collett <ralph.collett@gmail.com>
2022-04-07 15:31:27 +0100

    Refactor

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,24 +3,25 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
+        String lowercaseQuery = query.toLowerCase();
+        if (lowercaseQuery.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("your name")) {
+        if (lowercaseQuery.contains("your name")) {
             return "JavaWeb-GR";
         }
-        if (query.toLowerCase().contains("which of the following numbers is the largest")) {
-            String[] numbers = query.toLowerCase().split(":")[2].split(", ");
-            int i = 0;
+        if (lowercaseQuery.contains("which of the following numbers is the largest")) {
+            String[] numbers = lowercaseQuery.split(":")[2].split(", ");
+            int largestNumber = Integer.MIN_VALUE;
             for (String number : numbers) {
                 int parsed = Integer.parseInt(number.trim());
-                i = Math.max(i, parsed);
+                largestNumber = Math.max(largestNumber, parsed);
             }
-            return String.valueOf(i);
+            return String.valueOf(largestNumber);
         }
-        if (query.toLowerCase().contains("plus")) {
+        if (lowercaseQuery.contains("plus")) {
             String[] words = query.split(" ");
             int firstNumber = Integer.parseInt(words[words.length - 3]);
             int secondNumber = Integer.parseInt(words[words.length - 1]);

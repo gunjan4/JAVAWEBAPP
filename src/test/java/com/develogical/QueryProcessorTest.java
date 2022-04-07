0cffd321f5c5cf4af1e3da4cb50352c9c167a5e7
Gunjan <gunjan \.kothari4@gmail.com>
2022-04-07 14:45:12 +0100

    Fixed my name issue

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,9 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void whatIsMyNameKnown() throws Exception {
+        assertThat(queryProcessor.process("my name"), containsString("JavaWeb-GR"));
+    }
 }
